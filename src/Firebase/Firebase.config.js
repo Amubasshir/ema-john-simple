fc4745d91395013628e1ce7824f2d001ae0d0671
Amubasshir <themubasshir07@gmail.com>
2021-10-10 11:38:56 +0600

    logn and registation added

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDS_kpbWGZ1lOpnK9-UESACtolVX0eckMg",
+    authDomain: "ema-john-simple-c3df9.firebaseapp.com",
+    projectId: "ema-john-simple-c3df9",
+    storageBucket: "ema-john-simple-c3df9.appspot.com",
+    messagingSenderId: "154597639115",
+    appId: "1:154597639115:web:adea8a1fdac2d273b96c4e"
+};
+  
+export default firebaseConfig;
